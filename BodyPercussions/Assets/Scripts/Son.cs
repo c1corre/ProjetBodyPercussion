f8ff181efc903a356e0366df6bc95606fd169aef
Gautier <g1picard@enib.fr>
2015-09-30 08:27:03 +0200

    Gautier

--- BodyPercussions/Assets/Scripts/Son.cs
+++ BodyPercussions/Assets/Scripts/Son.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class Son : MonoBehaviour {
+public class Sound : MonoBehaviour {
 
+	private AudioClip[] clipArray;
+	public List<AudioClip> clips = new List<AudioClip>();
 	// Use this for initialization
 	void Start () {
-	
+		clipArray = Sound.LoadAll<AudioClip>("Sounds");
+		foreach(AudioClip son in clipArray){
+			clips.Add(son);
+		}
 	}
 	
 	// Update is called once per frame
